6e83c0052e12f5971184ee32cb5a3463ec3efb92
Boris <72118807+Curis-lab@users.noreply.github.com>
2024-02-14 20:45:42 +0630

    remove file

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,25 @@
+"use client";
+
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
 const Navbar = () => {
+  const {status} = useSession();
+
   return (
     <div className="flex justify-between pb-4 border-b mb-4">
       <div className="">
         <Link href={"/"}>
-          <h1 className="text-dark text-4xl font-bold tracking-tighter">Tech News</h1>
+          <h1 className="text-dark text-4xl font-bold tracking-tighter">
+            Tech News
+          </h1>
         </Link>
         <p className="text-sm">
           Exploring tomorrow's Invotions,
           <br /> One Byte at a Time.
         </p>
       </div>
+      {status === 'authenticated'? <div>Authen</div>:<div>UnAunti</div>}
       <div className="flex items-center">
         <Link className="btn" href={"/sign-in"}>
           Sign In
