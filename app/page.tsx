e63168522cb01dda603db7c6aa56f1a86e2c82e0
Boris <72118807+Curis-lab@users.noreply.github.com>
2024-02-14 08:24:37 +0630

    add post page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
 import CategoriesList from "@/components/CategoriesList";
+import Post from "@/components/Post";
+import { postsData } from "@/data";
 
 export default function Home() {
-  return <>
-  <CategoriesList/>
-  <div>Post</div>
-    </>;
+  return (
+    <>
+      <CategoriesList />
+      {postsData && postsData.length > 0 ? (
+        postsData.map((post) => <Post 
+        key={post.id}
+        id={post.id}
+        author = {post.author}
+        authorEmail={"test@email.com"}
+        date={post.datepublished}
+        thumbnail={post.thumbnail}
+        category={post.category}
+        title={post.title}
+        content={post.content}
+        links={post.links || []}/>)
+      ) : (
+        <div>No Posts to display</div>
+      )}
+    </>
+  );
 }
