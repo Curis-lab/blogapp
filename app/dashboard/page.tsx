d9f49c1c3bfcc7b948f4ccde2ad66abbdf7fcc5e
Boris <72118807+Curis-lab@users.noreply.github.com>
2024-02-14 08:58:47 +0630

    finished dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,29 @@
-export default function Dashboard(){
-    return(
-        <div>
-            <h1>My Posts</h1>
+import CategoriesList from "@/components/CategoriesList";
+import Post from "@/components/Post";
+import { postsData } from "@/data";
+import Link from "next/link";
+
+export default function Dashboard() {
+  return (
+    <>
+      <CategoriesList />
+      {postsData && postsData.length > 0 ? (
+        postsData.map((post) => <Post 
+        key={post.id}
+        id={post.id}
+        author = {post.author}
+        authorEmail={"test@email.com"}
+        date={post.datepublished}
+        thumbnail={post.thumbnail}
+        category={post.category}
+        title={post.title}
+        content={post.content}
+        links={post.links || []}/>)
+      ) : (
+        <div>No Post Created yet. 
+            <Link href={`/create-post`} className="underline">Create New Post</Link>
         </div>
-    )
+      )}
+    </>
+  );
 }
